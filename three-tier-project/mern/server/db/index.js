a7e1b17b0d762754bc2ba10205c44acee35b5b9e
santabun <santabun3530@gmail.com>
2025-07-29 17:13:43 +0600

    add to compose file

---
+++ three-tier-project/mern/server/db/index.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const connectionString = 'mongodb://mongo:27017/cinema';
+
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).catch((e) => {
+  console.error('Connection error', e.message);
+});
+
+const db = mongoose.connection;
+
+module.exports = db;
+
